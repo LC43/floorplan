4c1b7f8da4b3ab5b60a5b1ef81ae4175c1a12634
persefone <persefone@WhereAngelsFallSilent.localdomain>
2009-02-02 08:39:41 +0000

    done it right again ;)

--- others/draganddrop/draggableicons/svglistwidget.cpp
+++ others/draganddrop/draggableicons/svglistwidget.cpp
@@ -58,17 +58,6 @@ SvgListWidget::SvgListWidget(QWidget *parent)
 }
 void SvgListWidget::createDockWindows()
 {
-	QDockWidget *dock = new QDockWidget(tr("Customers"), this);
-	dock->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
-	customerList = new QListWidget(dock);
-	customerList->addItems(QStringList()
-			<< "John Doe, Harmony Enterprises, 12 Lakeside, Ambleton"
-			<< "Jane Doe, Memorabilia, 23 Watersedge, Beaton"
-			<< "Tammy Shea, Tiblanka, 38 Sea Views, Carlton"
-			<< "Tim Sheen, Caraba Gifts, 48 Ocean Way, Deal"
-			<< "Sol Harvey, Chicos Coffee, 53 New Springs, Eccleston"
-			<< "Sally Hobart, Tiroli Tea, 67 Long River, Fedula");
-	dock->setWidget(customerList);
 	//QMainWindow::addDockWidget(Qt::RightDockWidgetArea, dock);
 	//viewMenu->addAction(dock->toggleViewAction());
 }
