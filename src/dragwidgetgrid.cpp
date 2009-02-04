ae4d1641c010df777a20e6e4a16ff024d5242192
verde <leite.carvalho@gmail.com>
2009-02-04 05:27:43 +0000

    tried but failed to make the pixmap move

--- src/dragwidgetgrid.cpp
+++ src/dragwidgetgrid.cpp
@@ -132,29 +132,44 @@ void DragWidgetGrid::dropEvent(QDropEvent *event)
 
 void DragWidgetGrid::mousePressEvent(QMouseEvent *event)
 {
-
+	// get mouse position
+	QPointF mouse_pos = event->pos();
+	QList<QGraphicsItem *> block_list;
+	block_list = scene.items( mouse_pos );
+	if( block_list.empty() ){
+		qDebug() << "vazio!";
+		return;
+	}
+	qDebug() << " not vazio!";
+	// cast :O
+	/*QGraphicsPixmapItem *pixmap_item;
+	pixmap_item = static_cast<QGraphicsPixmapItem*>(block_list.first());
+	
+	*/
+/*
     QLabel *child = static_cast<QLabel*>(childAt(event->pos()));
     
     if (!child)
         return;
+	*/
 	/*
-    QPixmap pixmap = *child->pixmap();
-    */
+    QPixmap pixmap = pixmap_item->pixmap();
+    
 
     QByteArray itemData;
     QDataStream dataStream(&itemData, QIODevice::WriteOnly);
-    /*
-    dataStream << pixmap << QPoint(event->pos() - child->pos());
-*/
-/*
+    
+    dataStream << pixmap << QPoint(event->pos());
+
     QMimeData *mimeData = new QMimeData;
     mimeData->setData("application/x-dnditemdata", itemData);
-
+	
     QDrag *drag = new QDrag(this);
     drag->setMimeData(mimeData);
     drag->setPixmap(pixmap);
-    drag->setHotSpot(event->pos() - child->pos());
-
+    drag->setHotSpot(event->pos());
+	qDebug() << "is drag empty: " << (drag->pixmap()).isNull();
+	qDebug() << "mouse pos:" << event->pos();
 
     QPixmap tempPixmap = pixmap;
     QPainter painter;
@@ -162,13 +177,15 @@ void DragWidgetGrid::mousePressEvent(QMouseEvent *event)
     painter.fillRect(pixmap.rect(), QColor(127, 127, 127, 127));
     painter.end();
 
-    child->setPixmap(tempPixmap);
+	// n percebo
+    //child->setPixmap(tempPixmap);
 
-    if (drag->exec(Qt::CopyAction | Qt::MoveAction, Qt::CopyAction) == Qt::MoveAction)
-        child->close();
+    if (drag->exec(Qt::CopyAction | Qt::MoveAction, Qt::CopyAction) == Qt::MoveAction){
+        //child->close();
+	}
     else {
-        child->show();
-        child->setPixmap(pixmap);
+        //child->show();
+        //child->setPixmap(pixmap);
     }
 	*/
 }
